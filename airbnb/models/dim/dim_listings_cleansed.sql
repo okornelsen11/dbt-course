cdf80829c043ab0d3bb7d9aa854fa0c4c8571821
Owen Kornelsen <okornelsen@redrivermutual.com>
2026-09-16 14:16:45 -0500

    add dims

--- airbnb/models/dim/dim_listings_cleansed.sql
+++ airbnb/models/dim/dim_listings_cleansed.sql
@@ -4,7 +4,6 @@ WITH src_listings AS (
 SELECT 
     listing_id,
     listing_name,
-    listing_url,
     room_type,
     CASE
         WHEN minimum_nights = 0 THEN 1
@@ -14,3 +13,5 @@ SELECT
     REPLACE( price_str, '$') :: NUMBER(10,2) AS price,
     created_at,
     updated_at
+FROM
+    src_listings
